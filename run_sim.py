a6efd23e2521ce621cf224dcac62c7b8e553a1d9
Zoë Farmer <zoe@dataleek.io>
2017-09-12 10:14:41 -0600

    Saving/Loading
    * `save()` function works
        * Will create `saves/` directory if it doesn't exist
        * Each savefile is formatted `gamestate_{isoformatdatetime}`
    * Loading *mostly* works....
        * Will not overwrite local scope arena for some reason (even with `nonlocal`)
        * Current use is `arena = load()` (good enough for now)

--- run_sim.py
+++ run_sim.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3.6
 
 import sys
+import os
+from datetime import datetime as dt
+import pickle
 from IPython import embed
 
 import kobayashi as kob
@@ -19,12 +22,49 @@ def main():
     show = arena.show
     list_ships = arena.list_ships
     tickn = arena.tickn
-    save = lambda: arena.show(save=True)
     help = arena.help
+    save = lambda: save_arena(arena)
+
+    def load():
+        nonlocal arena
+        arena = load_arena()
+        return arena
 
     embed()
 
 
+def save_arena(arena):
+    if not os.path.isdir('saves'):
+        os.mkdir('saves')
+
+    with open(f'saves/gamestate_{dt.isoformat(dt.now())}.pkl', 'wb') as savefile:
+        pickle.dump(arena, savefile, pickle.HIGHEST_PROTOCOL)
+
+
+def load_arena():
+    contents = [x for x in os.listdir('saves/')
+                if (os.path.isfile(os.path.join('saves', x)) and x.startswith('gamestate'))]
+    contents.sort(key=lambda f: os.path.getmtime(os.path.join('saves', f)))
+    for i, f in enumerate(contents):
+        print(f'({i + 1}) {f}')
+    while True:
+        choice = input(f'Please select a file to load (default {contents[-1]}) ')
+        try:
+            if choice == '':
+                choice = 0
+                break
+            choice = int(choice)
+            if choice < 1 or choice > len(contents):
+                raise ValueError
+            break
+        except ValueError:
+            print(f'Invalid input. Please enter a number between 1 and {len(contents)}')
+    file_to_load = os.path.join('saves', contents[choice - 1])
+
+    with open(file_to_load, 'rb') as savefile:
+        arena = pickle.load(savefile)
+
+    return arena
 
 
 if __name__ == '__main__':
